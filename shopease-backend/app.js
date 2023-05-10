b9d22a1b0a8bda3db89a47dfd3b899053b7acc60
wakeupzaman909 <hadiuzzaman909@outlook.com>
2023-05-10 19:36:49 +0600

    setup completed

--- shopease-backend/app.js
+++ shopease-backend/app.js
@@ -1,5 +1,13 @@
 const express=require("express");
+const ErrorHandler = require("./utils/ErrorHandler");
 const app=express();
+const cookieParser=require("cookie-parser");
+const fileUpload=require("express-fileupload");
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload({useTempFiles: true}));
 
 
 //config
@@ -9,5 +17,8 @@ if(process.env.NODE_ENV !== "PRODUCTION"){
     })
 }
  
+//errorHandling
+app.use(ErrorHandler)
+
 
 module.exports=app;
