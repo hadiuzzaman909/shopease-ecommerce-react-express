07e0235857c3f1d44bc73551bd5c33a3b075161d
wakeupzaman909 <hadiuzzaman909@outlook.com>
2023-05-16 13:24:52 +0600

    login and signup page completed

--- shopease-frontend/src/App.js
+++ shopease-frontend/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import {LoginPage, SignUpPage} from './routes/Routes';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-[#000]">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/signUp' element={<SignUpPage/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
